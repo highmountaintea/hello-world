2ea84948f869e8377b61c16fdad5aa692ee13871
johnliu <john.liu@karmadata.com>
2016-12-09 18:05:19 -0500

    typo fix

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get('/value', function(req, res) {
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port!')
+  console.log('Example app listening on port ' + port)
 })
