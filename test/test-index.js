96f8ef4136ab3127c2db8b66f8cd416370c13036
johnfliu <johnfliu@coolimage.com>
2016-12-09 22:10:41 -0500

    now let the test pass

--- test/test-index.js
+++ test/test-index.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('Main test', function() {
   describe('empty test()', function() {
     it('should return fail', function() {
-      assert.equal(-1, 2+3);
+      assert.equal(5, 2+3);
     });
   });
 });
